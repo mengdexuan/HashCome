2f570019e71446a242bc376ad183238b65d0e681
mengdexuan <mengdexuan@itangquan.com>
2022-07-01 17:12:36 +0800

    test

--- contract-swap-api/src/main/java/com/bizzan/bitrade/Test.java
+++ contract-swap-api/src/main/java/com/bizzan/bitrade/Test.java
@@ -1,12 +1,35 @@
 package com.bizzan.bitrade;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
     public static void main(String[] args) {
-        String tem = "btcusdt";
-        StringBuilder sb = new StringBuilder(tem);
-        sb.insert(tem.indexOf("usdt"), "/");
-        System.out.println(sb.toString().toUpperCase());
+//        String url = "http://103.218.242.25:8801/swap/order/open";
+//        String url = "http://103.218.242.25:8801/uc/login";
+        String url = "http://103.218.242.25:8801/swap/order/ajust-principal";
+
+        HttpRequest post = HttpUtil.createPost(url);
+        post.header("x-auth-token","5d8a3808-c294-4970-b630-a6228cfaa16a");
+
+        Map<String,Object> param = new HashMap<>();
+        param.put("type","1"); //0:增加  1：减少
+        param.put("principal","10");
+        param.put("contractCoinId","1");
+        param.put("direction","1");
+
+        post.form(param);
+
+        String result = post.execute().body();
+
+//        String result = HttpUtil.post(url, param);
+
+        System.out.println(result);
+
     }
 
 
