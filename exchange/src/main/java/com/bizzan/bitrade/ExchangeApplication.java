adffa7699680e2c242a772958e3ab327df08eccd
mengdexuan <mengdexuan@itangquan.com>
2022-07-09 11:16:29 +0800

    test

--- exchange/src/main/java/com/bizzan/bitrade/ExchangeApplication.java
+++ exchange/src/main/java/com/bizzan/bitrade/ExchangeApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
