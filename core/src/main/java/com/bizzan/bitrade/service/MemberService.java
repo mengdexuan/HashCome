3c75ff16151ddcb2cd08e989ea0b6dfc5d35cc60
晁高锋 <chaogaofeng@avic-intl.cn>
2022-08-18 17:19:45 +0800

    fix

--- core/src/main/java/com/bizzan/bitrade/service/MemberService.java
+++ core/src/main/java/com/bizzan/bitrade/service/MemberService.java
@@ -221,7 +221,6 @@ public class MemberService extends BaseService {
             log.error("can't set token expire");
             return null;
         }
-        log.info(thirdVerifyUrl+":"+member.getEmail());
 
         return member;
     }
